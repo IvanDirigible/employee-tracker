69ab4253d2f001889eb520599f3044aa2c944a5e
IvanDirigible <sickeruncible@gmail.com>
2023-11-30 19:27:20 -0500

    Making progress on figuring our how to properly pull info into employee table.

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,11 +15,11 @@ VALUES  ('Sales Lead', 100000, 1),
         ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('John', 'Doe', 1),
-        ('Mike', 'Chan', 2, 'John Doe'),
-        ('Ashley', 'Rodriguez', 3),
-        ('Kevin', 'Tupik', 4, 'Ashley Rodriguez'),
-        ('Kunal', 'Singh', 5),
-        ('Malia', 'Brown', 6, 'Kunal Singh'),
-        ('Sarah', 'Lourd', 7),
-        ('Tom', 'Allen', 8, 'Sarah Lourd');
+VALUES  ('John', 'Doe', 1, NULL),
+        ('Mike', 'Chan', 2, 1),
+        ('Ashley', 'Rodriguez', 3, NULL),
+        ('Kevin', 'Tupik', 4, 3),
+        ('Kunal', 'Singh', 5, NULL),
+        ('Malia', 'Brown', 6, 5),
+        ('Sarah', 'Lourd', 7, NULL),
+        ('Tom', 'Allen', 8, 7);
--- index2.js
+++ index2.js
@@ -13,22 +13,31 @@ const mainMenu = function() {
                 type: 'list',
                 name: 'main',
                 message: 'Please select from the following options:',
-                choices: ['View All Departments', 'View All Roles', 'View All Employees', new inquirer.Separator(), 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role', new inquirer.Separator()]
+                choices:    ['View All Departments', 
+                            'View All Roles',
+                            'View All Employees',new inquirer.Separator(),
+                            'Add a Department',
+                            'Add a Role',
+                            'Add an Employee',
+                            'Update an Employee Role', new inquirer.Separator()]
             }
         ])
     .then((userRes) => {
         if (userRes.main === 'View All Departments') {
             viewDepartments();
-        } else if (userRes.main === 'View All Roles') {
+        } if (userRes.main === 'View All Roles') {
             viewRoles();
-        } else if (userRes.main === 'View All Employees') {
+        } if (userRes.main === 'View All Employees') {
             viewEmployees();
+        } if (userRes.main === 'Add a Department') {
+            addDepartment();
         }
     })
 };
 
 viewDepartments = function() {
     db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) throw err;
         console.log('Viewing All Departments');
         console.table(res);
         mainMenu();
@@ -36,7 +45,13 @@ viewDepartments = function() {
 };
 
 viewRoles = function() {
-    db.query(`SELECT * FROM role`, (err, res) => {
+    db.query(`SELECT role.id,
+            title,
+            department.name AS department,
+            role.salary
+                FROM role
+            JOIN department ON role.department_id = department.id`, (err, res) => {
+        if (err) throw err;
         console.log('Viewing All Roles');
         console.table(res);
         mainMenu();
@@ -44,9 +59,24 @@ viewRoles = function() {
 };
 
 viewEmployees = function() {
-    db.query(`SELECT * FROM employee`, (err, res) => {
+    db.query(`SELECT employee.id,
+            first_name,
+            last_name, 
+            role.title AS title,
+            department.name AS department,
+            role.salary,
+            CONCAT (employee.first_name, " ", employee.last_name) AS manager
+                FROM employee
+            JOIN role ON employee.role_id = role.id
+            JOIN department on employee.department.name = department.id
+            JOIN manager ON employee.manager_id = employee.id`, (err, res) => {
+        if (err) throw err;
         console.log('Viewing All Employees');
         console.table(res);
         mainMenu();
     })
 };
+
+addDepartment = function() {
+    db
+}
